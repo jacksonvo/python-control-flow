df49d9e9abed3a5eeb71cad93eb6dc01d5d04682
Jackson Vo <jacksonvo@ucla.edu>
2015-01-29 15:09:19 +0000

    This is homework 4.6.1 in scb p 51.

---
+++ exercise_1.py
@@ -0,0 +1,11 @@
+1) print hello after 3 to 17. so it would print it 14 times.
+
+2)The range is 12, % means modulo, so 12 divided by 3 will give 4. It will print hello 4 times.
+
+3)Considering that both are will print 3 hellos, then total would be 6 hellos.
+
+4)I would say it would print hello 3 times, because 0 + 3.
+
+5)First will print 7 and adding 1 more will give 8 "hello".
+
+What does the fooXX do?) do not understand.
